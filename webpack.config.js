f36a3b8723ab5d2a6300199f4566897276503648
Paul Hultgren <pjhultgren5@gmail.com>
2021-01-22 19:52:24 -0800

    Add HTML injection

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
+"use strict";
+
 const path = require("path");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          "babel-loader",
-          "ts-loader",
-        ],
-        exclude: /node_modules/,
-      },
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = function (env) {
+  return {
+    entry: "./src/index.tsx",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            "babel-loader",
+            "ts-loader",
+          ],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(__dirname, "public/index.html"),
+        favicon: "./public/favicon.png"
+      })
     ],
-  },
-  resolve: {
-    extensions: [ ".tsx", ".ts", ".js" ],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+    resolve: {
+      extensions: [ ".tsx", ".ts", ".js" ],
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+  };
 }
